56e582c124d87efa6da13601bdabf387a3570db4
Kieron Gillingham <kierongillingham@gmail.com>
2021-10-19 19:56:42 +0100

    parsing input filename

--- projects/06 - Assembler/c++/hackassembler.cpp
+++ projects/06 - Assembler/c++/hackassembler.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <string>
 using namespace std;
 
 /**
@@ -15,20 +16,33 @@ int main(int argc, char *argv[]) {
     // Start program execution
     cout << "Starting." << endl;
 
-    // List program arguments
+    // Handle program arguments
     if (argc != 0) {
+
+        // List arguments for debugging
         cout << argc << " arguments detected:" << endl;
         for (int i = 0; i < argc; i++) {
             cout << "  " << argv[i] << endl;
         }
+
     } else {
         cout << "No arguments detected." << endl;
     }
+
+    // Parse program arguments
+    // Handle input file
+    string filepath = "file.asm"; // Default input filepath
+    // Get new filepath if given as argument
+    if (argc > 1) {
+        char* arg = argv[1];
+        filepath = arg;
+    }
+    cout << "Assembling " << filepath << endl; // Output filepath to be used
 
     // TODO - Transfer assembly operation from Java project
     cout << "ERROR - Assembly not implemented." << endl;
 
     // End program execution
-    cout << "Stopping.";
+    cout << "Stopping." << endl;
     return 0;
 }
